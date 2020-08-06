fc39c5860799848d35b63b4d5424681fd144133b
Dominik Maier <d.maier@avm.de>
2020-08-06 13:24:35 +0200

    afl-returns

---
+++ include/afl-returns.h
@@ -0,0 +1,15 @@
+#ifndef AFL_RETURNS_H
+#define AFL_RETURNS_H
+
+typedef enum afl_ret {
+
+  AFL_RET_SUCCESS,
+  AFL_RET_ALLOC,
+  AFL_RET_FILE_OPEN,
+  AFL_RET_FILE_SIZE,
+  AFL_RET_SHORT_READ,
+  AFL_RET_ARRAY_END,
+
+} afl_ret_t;
+
+#endif
