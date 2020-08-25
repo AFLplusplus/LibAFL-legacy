a69f8dbbd81a9b68595c8766846533acaefe2ef5
Dominik Maier <domenukk@gmail.com>
2020-08-25 19:48:31 +0200

    added llmp unit

---
+++ test/unit_llmp.c
@@ -0,0 +1,79 @@
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <assert.h>
+#include <cmocka.h>
+#include <unistd.h>
+#include <fcntl.h>
+/* cmocka < 1.0 didn't support these features we need */
+#ifndef assert_ptr_equal
+  #define assert_ptr_equal(a, b)                                      \
+    _assert_int_equal(cast_ptr_to_largest_integral_type(a),           \
+                      cast_ptr_to_largest_integral_type(b), __FILE__, \
+                      __LINE__)
+  #define CMUnitTest UnitTest
+  #define cmocka_unit_test unit_test
+  #define cmocka_run_group_tests(t, setup, teardown) run_tests(t)
+#endif
+
+extern void mock_assert(const int result, const char *const expression,
+                        const char *const file, const int line);
+#undef assert
+#define assert(expression) \
+  mock_assert((int)(expression), #expression, __FILE__, __LINE__);
+
+/* remap exit -> assert, then use cmocka's mock_assert
+    (compile with `--wrap=exit`) */
+extern void exit(int status);
+extern void __real_exit(int status);
+void        __wrap_exit(int status);
+void        __wrap_exit(int status) {
+
+  assert(0);
+
+}
+
+/* ignore all printfs */
+#undef printf
+extern int printf(const char *format, ...);
+extern int __real_printf(const char *format, ...);
+int        __wrap_printf(const char *format, ...);
+int        __wrap_printf(const char *format, ...) {
+
+  return 1;
+
+}
+
+#include "llmp.h"
+
+static void test_llmp_client(void **state) {
+
+
+  llmp_client_state_t *client = llmp_client_new_unconnected();
+  llmp_client_alloc_next(client, LLMP_INITIAL_MAP_SIZE + 10);
+
+  // Make sure larger allocations work and create a new map :)
+  assert_int_equal(client->out_map_count, 2);
+  llmp_client_destroy(client);
+
+}
+
+
+
+int main(int argc, char **argv) {
+
+  const struct CMUnitTest tests[] = {
+
+      cmocka_unit_test(test_llmp_client),
+
+  };
+
+  // return cmocka_run_group_tests (tests, setup, teardown);
+  __real_exit(cmocka_run_group_tests(tests, NULL, NULL));
+
+  // fake return for dumb compilers
+  return 0;
+
+}
+
