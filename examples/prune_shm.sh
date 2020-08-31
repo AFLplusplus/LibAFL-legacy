387a1665b4f7ef921589e30976ce54b39d70023f
Dominik Maier <domenukk@gmail.com>
2020-08-31 23:32:08 +0200

    prune shm

--- examples/prune_shm.sh
+++ examples/prune_shm.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-ipcrm shm $(ipcs -m | tail -n +4 | cut -d ' ' -f2) 2>/dev/null || echo "No maps found"
+ipcrm -m $(ipcs -m | tail -n +4 | cut -d ' ' -f2-3) 2>/dev/null || echo "No maps found"
+#on mac: ipcrm shm $(ipcs -m | tail -n +4 | cut -d ' ' -f2) 2>/dev/null || echo "No maps found"
