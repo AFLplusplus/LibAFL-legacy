5832cada707acf97b28009e0c82c12a76c82d13c
Dominik Maier <domenukk@gmail.com>
2020-09-09 16:56:13 +0200

    removed linked list for now

--- examples/forking-fuzzer.c
+++ examples/forking-fuzzer.c
@@ -44,7 +44,6 @@ llmp_broker_state_t *llmp_broker;
 int                  broker_port;
 
 /* A global array of all the registered engines */
-pthread_mutex_t fuzz_worker_array_lock;
 engine_t *      registered_fuzz_workers[MAX_WORKERS];
 u64             fuzz_workers_count;
 
@@ -339,24 +338,17 @@ void *run_broker_thread(void *data) {
 
 }
 
-/* Function to register/add a fuzz worker (engine). To avoid race condition, add
- * mutex here(Won't be performance problem). */
+/* Function to register/add a fuzz worker (engine). */
 static inline afl_ret_t afl_register_fuzz_worker(engine_t *engine) {
-
-  // Critical section. Needs a lock. Called very rarely, thus won't affect perf.
-  pthread_mutex_lock(&fuzz_worker_array_lock);
 
   if (fuzz_workers_count >= MAX_WORKERS) {
 
-    pthread_mutex_unlock(&fuzz_worker_array_lock);
     return AFL_RET_ARRAY_END;
 
   }
 
   registered_fuzz_workers[fuzz_workers_count] = engine;
   fuzz_workers_count++;
-  // Unlock the mutex
-  pthread_mutex_unlock(&fuzz_worker_array_lock);
   return AFL_RET_SUCCESS;
 
 }
