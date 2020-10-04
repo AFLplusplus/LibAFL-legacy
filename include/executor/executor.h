c07fdbd3e63d1c95c7d9ef004d06cb89b765e6b5
Andrea Fioraldi <andreafioraldi@gmail.com>
2020-10-04 11:34:34 +0200

    executor header

---
+++ include/executor/executor.h
@@ -0,0 +1,100 @@
+/*
+   american fuzzy lop++ - fuzzer header
+   ------------------------------------
+
+   Originally written by Michal Zalewski
+
+   Now maintained by Marc Heuse <mh@mh-sec.de>,
+                     Heiko Eißfeldt <heiko.eissfeldt@hexco.de>,
+                     Andrea Fioraldi <andreafioraldi@gmail.com>,
+                     Dominik Maier <mail@dmnk.co>
+
+   Copyright 2016, 2017 Google Inc. All rights reserved.
+   Copyright 2019-2020 AFLplusplus Project. All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at:
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   This is the Library based on AFL++ which can be used to build
+   customized fuzzers for a specific target while taking advantage of
+   a lot of features that AFL++ already provides.
+
+ */
+
+#ifndef LIBAFL_EXECUTOR_EXECUTOR_H
+#define LIBAFL_EXECUTOR_EXECUTOR_H
+
+#include "object.h"
+#include "returns.h"
+
+#include "observation_channel/observation_channel.h"
+#include "input/input.h"
+
+struct afl_executor_vtable {
+
+  /*
+    The destroy() method is optional.
+    It is invoked just before the destroy of the object.
+  */
+  u8 (*destroy)(afl_executor_t *);
+
+  /*
+    The run_target() method is mandatory.
+  */
+  afl_exit_t (*run_target)(afl_executor_t *);
+
+  /*
+    The run_target() method is mandatory.
+  */
+  u8 (*place_input)(afl_executor_t *, afl_input_t *);
+
+};
+
+/*
+  An Executor is an entity with a set of violation oracles, a set of observation channels, a function that allows
+  instructing the SUT about the input to test, and a function to run the SUT.
+*/
+struct afl_executor {
+
+  INHERITS(afl_object)
+
+  afl_observation_channel_t *observation_channels;
+  u32                        observation_channels_count;
+
+  afl_oracle_t *oracles;
+  u32           oracles_count;
+
+  afl_input_t *current_input;
+
+  struct afl_executor_vtable v;
+
+};
+
+/*
+  Initialize an empty, just allocated, afl_executor_t object.
+*/
+afl_ret_t afl_executor_init(afl_executor_t *);
+
+/*
+  Destroy an afl_executor_t object, you must call this method before releasing
+  the memory used by the object.
+*/
+void afl_executor_destroy(afl_executor_t *);
+
+/*
+  Add an afl_observation_channel_t to the list.
+*/
+afl_ret_t afl_executor_add_observation_channel(afl_executor_t *, afl_observation_channel_t *);
+
+/*
+  Reset the state of all the observation channels.
+*/
+void afl_executor_reset_observation_channels(afl_executor_t *);
+
+AFL_NEW_AND_DELETE_FOR(afl_executor)
+
+#endif
+
---
+++ include/object.h
@@ -0,0 +1,76 @@
+/*
+   american fuzzy lop++ - fuzzer header
+   ------------------------------------
+
+   Originally written by Michal Zalewski
+
+   Now maintained by Marc Heuse <mh@mh-sec.de>,
+                     Heiko Eißfeldt <heiko.eissfeldt@hexco.de>,
+                     Andrea Fioraldi <andreafioraldi@gmail.com>,
+                     Dominik Maier <mail@dmnk.co>
+
+   Copyright 2016, 2017 Google Inc. All rights reserved.
+   Copyright 2019-2020 AFLplusplus Project. All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at:
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   This is the Library based on AFL++ which can be used to build
+   customized fuzzers for a specific target while taking advantage of
+   a lot of features that AFL++ already provides.
+
+ */
+
+#ifndef LIBAFL_OBJECT_H
+#define LIBAFL_OBJECT_H
+
+#include "types.h"
+
+#define INHERITS(name) struct name base;
+#define BASE_CAST(obj) ((obj)->base)
+
+#define AFL_NEW_AND_DELETE_FOR(init_type) AFL_NEW_AND_DELETE_FOR_WITH_PARAMS(init_type, AFL_DECL_PARAMS(void), ret)
+
+/*
+This makro wraps all our afl_ ... _init and _deinit calls with _new and _delete wrappers.
+The _new wrapper allocates memory, and return NULL or the pointer, depending on result.
+The _delete wrapper calls _denit and deallocates the pointer, as created by _new.
+For decl and call, use AFL_DECL/CALL_PARAMS
+*/
+#define AFL_NEW_AND_DELETE_FOR_WITH_PARAMS(init_type, decl_params, call_params)      \
+  static inline init_type##_t *init_type##_new(decl_params) {                        \
+                                                                                     \
+    /*printf("Allocating " #init_type " with decl_params " #decl_params */           \
+    /*" and call params " #call_params " and size %ld\n", sizeof(init_type##_t) );*/ \
+    init_type##_t *ret = calloc(1, sizeof(init_type##_t));                           \
+    if (!ret) { return NULL; }                                                       \
+    if (init_type##_init(call_params) != AFL_RET_SUCCESS) {                          \
+                                                                                     \
+      free(ret);                                                                     \
+      return NULL;                                                                   \
+                                                                                     \
+    }                                                                                \
+    return ret;                                                                      \
+                                                                                     \
+  }                                                                                  \
+                                                                                     \
+  static inline void init_type##_delete(init_type##_t *init_type) {                  \
+                                                                                     \
+    init_type##_deinit(init_type);                                                   \
+    free(init_type);                                                                 \
+                                                                                     \
+  }
+
+#define AFL_DECL_PARAMS(...) __VA_ARGS__
+#define AFL_CALL_PARAMS(...) ret, __VA_ARGS__
+
+struct afl_object {
+
+  void* wrapper;
+
+};
+
+#endif
