e319b3eab45eb1e6cf381c26ac8c9bec0f663602
Andrea Fioraldi <andreafioraldi@gmail.com>
2020-10-07 15:14:41 +0200

    object header

--- include/object.h
+++ include/object.h
@@ -27,53 +27,103 @@
 #ifndef LIBAFL_OBJECT_H
 #define LIBAFL_OBJECT_H
 
-#include "types.h"
+#include "alloc.h"
 
-#define INHERITS(name) struct name base;
-#define BASE_CAST(obj) ((obj)->base)
-#define INSTANCE_OF(type, ptr) ((ptr)->v == &type##_vtable_instance)
+#define AFL_INHERITS(type) struct type _base;
+#define AFL_BASEOF(ptr) (&(ptr)->_base)
+
+#define AFL_VTABLE_INHERITS(type) struct type##_vtable _base;
+#define AFL_VTABLE_INIT_BASE_VPTR(type) ._base_vptr = (struct afl_object_vtable *)&type##_vtable_instance
+
+#define AFL_VTABLEOF(type, ptr) ((struct type##_vtable *)(((struct afl_object *)(ptr))->vptr))
+
+#define AFL_VTABLE_SET(ptr, vtable) ((struct afl_object *)(ptr))->vptr = (struct afl_object_vtable *)&vtable
+
+// #define INSTANCE_OF(type, ptr) ((ptr)->v == &type##_vtable_instance)
+
+#define AFL_INSTANCEOF(type, ptr)                                                       \
+  ({                                                                                    \
+                                                                                        \
+    struct afl_object_vtable *_v = (struct afl_object_vtable *)&type##_vtable_instance; \
+    while (_v) {                                                                        \
+                                                                                        \
+      if (_v == ((struct afl_object *)(ptr))->vptr) break;                              \
+      _v = _v->_base_vptr;                                                              \
+                                                                                        \
+    }                                                                                   \
+    !!_v;                                                                               \
+                                                                                        \
+  })
+
+#define AFL_DYN_CAST(type, ptr) (INSTANCE_OF(type, ptr) ? (struct type *)(ptr) : NULL)
 
 #define AFL_DECL_PARAMS(...) __VA_ARGS__
 #define AFL_CALL_PARAMS(...) ret, __VA_ARGS__
 
-#define AFL_NEW_FOR(init_type) AFL_NEW_FOR_WITH_PARAMS(init_type, AFL_DECL_PARAMS(void), ret)
-
-/*
-This makro wraps all our afl_ ... _init and _deinit calls with _new and _delete wrappers.
-The _new wrapper allocates memory, and return NULL or the pointer, depending on result.
-The _delete wrapper calls _denit and deallocates the pointer, as created by _new.
-For decl and call, use AFL_DECL/CALL_PARAMS
-*/
-#define AFL_NEW_FOR_WITH_PARAMS(init_type, decl_params, call_params)      \
-  static inline init_type##_t *init_type##_new(decl_params) {                        \
-                                                                                     \
-    /*printf("Allocating " #init_type " with decl_params " #decl_params */           \
-    /*" and call params " #call_params " and size %ld\n", sizeof(init_type##_t) );*/ \
-    init_type##_t *ret = calloc(1, sizeof(init_type##_t));                           \
-    if (!ret) { return NULL; }                                                       \
-    if (init_type##_init(call_params) != AFL_RET_SUCCESS) {                          \
-                                                                                     \
-      free(ret);                                                                     \
-      return NULL;                                                                   \
-                                                                                     \
-    }                                                                                \
-    return ret;                                                                      \
-                                                                                     \
-  }                                                                                  \
-
-
-#define AFL_DELETE_FOR(init_type)  \
-  static inline void init_type##_delete(init_type##_t *init_type) {                  \
-                                                                                     \
-    init_type##_deinit(init_type);                                                   \
-    free(init_type);                                                                 \
-                                                                                     \
+#define AFL_NEW_FOR_WITH_PARAMS(type, decl_params, call_params) \
+  static inline struct type *type##_new(decl_params) {          \
+                                                                \
+    struct type *ret = afl_alloc(sizeof(struct type));          \
+    if (!ret) { return NULL; }                                  \
+    if (type##_init(call_params) != AFL_RET_SUCCESS) {          \
+                                                                \
+      afl_free(ret);                                            \
+      return NULL;                                              \
+                                                                \
+    }                                                           \
+    return ret;                                                 \
+                                                                \
   }
 
-struct afl_object {
+#define AFL_NEW_FOR(type) AFL_NEW_FOR_WITH_PARAMS(type, AFL_DECL_PARAMS(void), ret)
 
-  void* wrapper;
+#define AFL_DELETE_FOR(type)                           \
+  static inline void type##_delete(struct type *obj) { \
+                                                       \
+    type##_deinit(obj);                                \
+    afl_free(obj);                                     \
+                                                       \
+  }
+
+typedef struct afl_object afl_object_t;
+
+struct afl_object_vtable {
+
+  struct afl_object_vtable *_base_vptr;
+
+  void *(*_create_wrapper)(void *);  // for bindings
+
+  /*
+    The deinit() method is optional.
+    It is invoked just before the destroy of the object.
+  */
+  void (*deinit)(afl_object_t *);
 
 };
 
+struct afl_object_vtable afl_object_vtable_instance = {
+
+    ._base_vptr = NULL,
+
+    .deinit = NULL
+
+};
+
+struct afl_object {
+
+  void *wrapper;  // for bindings
+
+  struct afl_object_vtable *vptr;
+
+};
+
+static inline void afl_object_deinit(afl_object_t *self) {
+
+  if (VTABLE_OF(afl_object, self)->deinit) VTABLE_OF(afl_object, self)->deinit(self);
+
+}
+
+AFL_DELETE_FOR(afl_object)
+
 #endif
+
