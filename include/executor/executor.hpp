ad97317b1d6e6f1128f55d92bd44071e2caad700
Andrea Fioraldi <andreafioraldi@gmail.com>
2020-10-08 14:39:24 +0200

    cpp executor header

---
+++ include/executor/executor.hpp
@@ -0,0 +1,64 @@
+/*
+   american fuzzy lop++ - fuzzer header
+   ------------------------------------
+
+   Originally written by Michal Zalewski
+
+   Now maintained by Marc Heuse <mh@mh-sec.de>,
+                     Heiko Eißfeldt <heiko.eissfeldt@hexco.de>,
+                     Andrea Fioraldi <andreafioraldi@gmail.com>,
+                     Dominik Maier <mail@dmnk.co>
+
+   Copyright 2016, 2017 Google Inc. All rights reserved.
+   Copyright 2019-2020 AFLplusplus Project. All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at:
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   This is the Library based on AFL++ which can be used to build
+   customized fuzzers for a specific target while taking advantage of
+   a lot of features that AFL++ already provides.
+
+ */
+
+#ifndef LIBAFL_EXECUTOR_EXECUTOR_H
+#define LIBAFL_EXECUTOR_EXECUTOR_H
+
+#include <vector>
+
+#include "error.hpp"
+
+#include "observation_channel/observation_channel.hpp"
+#include "input/input.hpp"
+
+/*
+  An Executor is an entity with a set of violation oracles, a set of observation channels, a function that allows
+  instructing the SUT about the input to test, and a function to run the SUT.
+*/
+class Executor {
+
+  std::vector<ObservationChannel*> observation_channels;
+  
+  Input* current_input;
+  
+public:
+
+  /*
+    Run the target represented by the executor.
+  */
+  virtual ExitType run_target() = 0;
+  
+  /*
+    Instruct the SUT about the input.
+  */
+  virtual u8 place_input(Input* input) {
+    current_input = input;
+  }
+
+};
+
+#endif
+
